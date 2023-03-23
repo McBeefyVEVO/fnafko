5494bc4bd25adbd4d1360e6c30ddc593d3232895
McBeefyVEVO <ondrastepo@gmail.com>
2023-03-23 08:59:31 +0100

    0.1.1

--- res/js/main.js
+++ res/js/main.js
@@ -1,5 +1,13 @@
 const start = document.getElementById("start");
+const main_title = document.getElementById("main_title");
+const forsen = document.getElementById("forsen");
 
 start.onclick = () => {
-    start.style.display = "block";
+    document.body.style.background = "white";
+
+    start.style.display = "none";
+    main_title.style.display = "none";
+    forsen.style.display = "none";
+    body.style.background = "white"
+    
 }
