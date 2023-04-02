7317aabb0da38b9920b91bd4ab8b5493ef6cac30
McBeefyVEVO <ondrastepo@gmail.com>
2023-04-02 15:54:52 +0200

    v0.3

--- res/js/main.js
+++ res/js/main.js
@@ -7,11 +7,25 @@ const close_cam = document.getElementById("close_cam");
 
 const look_left = document.getElementById("look_left");
 const look_right = document.getElementById("look_right");
+const look_left_back = document.getElementById("look_left_back");
+const look_right_back = document.getElementById("look_right_back");
 
 let camera_on = 0;
+let leftopen = 1;
+let rightopen = 1;
+
+let lighton_left = 0;
+let lighton_right = 0;
+
+//------------------------------------------------------------------------------------------
+//Startík
 
 start.onclick = () => {
-    document.body.style.background = "white";
+    document.body.style.background = "url(./res/img/office.png)";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition = "center center";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
 
     start.style.display = "none";
     main_title.style.display = "none";
@@ -19,11 +33,10 @@ start.onclick = () => {
     open_cam.style.display = "block";
     look_left.style.display = "block";
     look_right.style.display = "block";
-
-    body.style.background = "white"
-    
-    
 }
+
+//------------------------------------------------------------------------------------------
+//Kamerky
 
 open_cam.onclick = () => {
     camera_on++;
@@ -44,5 +57,75 @@ close_cam.onclick = () => {
 }
 
 
+//------------------------------------------------------------------------------------------
+//Koukání doleva/doprava
 
 
+look_left.onclick = () => {
+    document.body.style.background = "url(./res/img/left_open.png)";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition = "center center";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+
+    if(leftopen == 0){
+    document.body.style.background = "url(./res/img/left_closed.png)";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition = "center center";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+    }
+
+    open_cam.style.display = "none";
+    look_left.style.display = "none";
+    look_right.style.display = "none";
+    look_left_back.style.display = "block";
+}
+
+look_right.onclick = () => {
+    document.body.style.background = "url(./res/img/right_open.png)";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition = "center center";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+
+    if(rightopen == 0){
+    document.body.style.background = "url(./res/img/right_closed.png)";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition = "center center";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+    }
+
+
+    open_cam.style.display = "none";
+    look_left.style.display = "none";
+    look_right.style.display = "none";
+    look_right_back.style.display = "block";
+}
+
+look_left_back.onclick = () => {
+    document.body.style.background = "url(./res/img/office.png)";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition = "center center";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+
+    open_cam.style.display = "block";
+    look_left.style.display = "block";
+    look_right.style.display = "block";
+    look_left_back.style.display = "none";
+}
+
+look_right_back.onclick = () => {
+    document.body.style.background = "url(./res/img/office.png)";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundPosition = "center center";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundAttachment = "fixed";
+
+    open_cam.style.display = "block";
+    look_left.style.display = "block";
+    look_right.style.display = "block";
+    look_right_back.style.display = "none";
+}
